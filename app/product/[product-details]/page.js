4dddf309013b6d0232170a998794a5c0351bcdf2
Ganesh Nawghare <ganesh7003nawghare@gmail.com>
2024-02-18 23:27:57 +0530

    added carousel

--- app/product/[product-details]/page.js
+++ app/product/[product-details]/page.js
@@ -5,77 +5,185 @@ import axios from "axios"
 import share from '/public/share.svg'
 import truck from '/public/Truck.svg'
 import pad from '/public/Pad.svg'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
 const ProductCard = ({ name, sizes, imageUrls, rs }) => {
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
   return (
-    <div className="flex justify-center w-full">
-      <div className="flex flex-row">
-        <div className="w-[53vw] ">
-          <div className="flex flex-wrap">
-            {imageUrls.map((url, index) => (
-              <div key={index} className="w-full sm:w-1/2 mb-1">
-                <img src={url} alt={name} className="w-full" />
-              </div>
-            ))}
+    <div className="flex flex-col">
+      <div className="flex justify-center w-full">
+        <div className="flex flex-row">
+          <div className="w-[53vw] ">
+            <div className="flex flex-wrap">
+              {imageUrls.map((url, index) => (
+                <div key={index} className="w-full sm:w-1/2 mb-1">
+                  <img src={url} alt={name} className="w-full" />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="w-[45vw] p-5">
-          <div className="flex justify-between items-center mb-4">
-          <h3 className="text-xl font-semibold font-Montserrat">{name}</h3>
-            <p className="text-xl font-semibold font-Montserrat">Rs {rs}</p>
-          </div>
-          <div className="flex items-center space-x-2 mb-4">
-            <div className="w-4 h-4 bg-[#5C6672]"></div>
-            <span className="text-gray-700 font-Montserrat">White</span>
-          </div>
-          <a href="#" className="text-grey-500 font-Montserrat"><u>Size Guide</u></a>
-          <div className="w-full h-[1px] bg-[#5C6672] bg-opacity-40 my-4" />
-          <div className="p-3">
-            {sizes.map((size, index) => (
-              <p className="mb-2" key={index}><u>{size}</u></p>
-            ))}
-          </div>
-          <div className="w-full h-[1px] bg-[#5C6672] bg-opacity-40 my-4" />
-          <div className="flex mb-2">
-            <button className="w-[38vw] bg-[#172332] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add to shopping bag</button>
-            <span className="flex w-[50px] h-[50px] ml-2 cursor-pointer flex items-center justify-center border border-gray-400"> <Image
+          <div className="w-[45vw] p-5">
+            <div className="flex justify-between items-center mb-4">
+              <h3 className="text-xl font-semibold font-Montserrat">{name}</h3>
+              <p className="text-xl font-semibold font-Montserrat">Rs {rs}</p>
+            </div>
+            <div className="flex items-center space-x-2 mb-4">
+              <div className="w-4 h-4 bg-[#5C6672]"></div>
+              <span className="text-gray-700 font-Montserrat">White</span>
+            </div>
+            <a href="#" className="text-grey-500 font-Montserrat"><u>Size Guide</u></a>
+            <div className="w-full h-[1px] bg-[#5C6672] bg-opacity-40 my-4" />
+            <div className="p-3">
+              {sizes.map((size, index) => (
+                <p className="mb-2" key={index}><u>{size}</u></p>
+              ))}
+            </div>
+            <div className="w-full h-[1px] bg-[#5C6672] bg-opacity-40 my-4" />
+            <div className="flex mb-2">
+              <button className="w-[38vw] bg-[#172332] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add to shopping bag</button>
+              <span className="flex w-[50px] h-[50px] ml-2 cursor-pointer flex items-center justify-center border border-gray-400"> <Image
                 src="/like.svg"
                 width={20}
                 height={20}
                 alt="Product details"
               /></span>
-          </div>
-          <div className="flex justify-between">
-            <div class="flex items-center space-x-2">
+            </div>
+            <div className="flex justify-between">
+              <div class="flex items-center space-x-2">
+                <Image
+                  src="/Pad.svg"
+                  width={20}
+                  height={20}
+                  alt="Product details"
+                />
+                <text>Product details</text>
+              </div>
+              <div class="flex items-center space-x-2">
+                <text>Share</text>
+                <Image
+                  src="/share.svg"
+                  width={20}
+                  height={20}
+                  alt="Product details"
+                />
+              </div>
+            </div>
+            <div class="flex items-center space-x-2 mt-3">
               <Image
-                src="/Pad.svg"
-                width={20}
-                height={20}
+                src="/Truck.svg"
+                width={30}
+                height={30}
                 alt="Product details"
               />
-              <text>Product details</text>
+              <text>Delivery & Returns</text>
             </div>
-            <div class="flex items-center space-x-2">
-              <text>Share</text>
-              <Image
-                src="/share.svg"
-                width={20}
-                height={20}
-                alt="Product details"
-              />
-            </div>
-          </div>
-          <div class="flex items-center space-x-2 mt-3">
-            <Image
-              src="/Truck.svg"
-              width={30}
-              height={30}
-              alt="Product details"
-            />
-            <text>Delivery & Returns</text>
           </div>
         </div>
+      </div>
+      <div className="flex">
+        <div className="p-7">
+          <h3 className="font-semibold font-Montserrat">Description:</h3>
+          <p className="mt-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <div className="flex space-x-2 mt-5">
+
+            <button className="w-[10vw] bg-[#D9D9D9] hover:bg-blue-700 text-white font-bold py-2 px-4">T shirts</button>
+            <button className="w-[20vw] bg-[#D9D9D9] hover:bg-blue-700 text-white font-bold py-2 px-4">Oversized tshirts</button>
+          </div>
+        </div>
+        <div className="p-7">
+          <h3 className="font-semibold font-Montserrat">Product Care</h3>
+          <p className="mt-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+      <div className="p-5">
+        <div>
+          <h3 className="font-semibold font-Montserrat">In the same style</h3>
+        </div>
+        <Carousel className="w-[95vw] mt-2" responsive={responsive} autoPlay={true}>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 1" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 2" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 3" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 3" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 3" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 3" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 3" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 3" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+          <div>
+            <img src="https://keonweb.s3.ap-south-1.amazonaws.com/abc.png" alt="Image 3" />
+            <div className="pl-4 pr-4">
+              <p className="mt-5">Plain White Tshirt</p>
+              <p className="mt-2">Rs: 1000</p>
+            </div>
+          </div>
+        </Carousel>
       </div>
     </div>
   );
@@ -84,10 +192,10 @@ const ProductCard = ({ name, sizes, imageUrls, rs }) => {
 export default function Category({ params }) {
   const [hoveredIndex, setHoveredIndex] = useState(null);
   // const [categoryData, setCategoryData] = useState([
-  //   { name: 'Plain White Tshirt', size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'], image: '/trending1.jpg', rs: '1000'},
-  //   { name: 'Plain White Tshirt', size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'], image: '/trending1.jpg', rs: '1000'},
-  //   { name: 'Plain White Tshirt', size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'], image: '/trending1.jpg', rs: '1000'},
-  //   { name: 'Plain White Tshirt', size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'], image: '/trending1.jpg', rs: '1000'}
+  //   {name: 'Plain White Tshirt', size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'], image: '/trending1.jpg', rs: '1000'},
+  //   {name: 'Plain White Tshirt', size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'], image: '/trending1.jpg', rs: '1000'},
+  //   {name: 'Plain White Tshirt', size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'], image: '/trending1.jpg', rs: '1000'},
+  //   {name: 'Plain White Tshirt', size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'], image: '/trending1.jpg', rs: '1000'}
   // ])
 
   const [categoryData, setCategoryData] = useState([
